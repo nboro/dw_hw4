f3cb87942b0489b6dcad240f473ef4f1e4b33e07
Nemania Borovits <nemaniaborovits@gmail.com>
2020-04-26 19:11:26 +0200

    solve 500 attempt 3

--- app.py
+++ app.py
@@ -1,24 +1,26 @@
 # import pandas as pd
 # import pickle
 import dash
-# import dash_core_components as dcc
 import dash_bootstrap_components as dbc
+import flask
 # import dash_html_components as html
 # from dash.dependencies import Input, Output
 # import plotly.graph_objs as go
 # from view import layout
 # from data import songs_bill_melt
 
+server = flask.Flask(__name__)
 app = dash.Dash(
     __name__,meta_tags=[{'name': 'viewport', 'content': 'width=device-width',
     # 'title':'Nemania Borovits | HW3 Data Visualization | MPG Dataset',
     # 'keywords':'Nemania Borovits, python dash, data visualization',
     # 'description':'Nemania Borovits made this simple python dash app as part of hw3 for data visualization course'
     }],
-    external_stylesheets=[dbc.themes.BOOTSTRAP]
+    external_stylesheets=[dbc.themes.BOOTSTRAP],
+    server = server
 )
 
-server = app.server
+# server = app.server
 app.title = 'HW4 Data Visualization | Top 2000 Dataset'
 # app.layout = layout
 app.config.suppress_callback_exceptions = True
