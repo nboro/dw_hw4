3c87181adfd38bfcf1d6f8a78d53d27f0267b80d
Yosef <yosefardhitowin@gmail.com>
2020-05-05 21:14:22 +0200

    update lyric chart to song scatter plot add new lyric data lyric.json

--- pages/lyrics.py
+++ pages/lyrics.py
@@ -1,216 +1,234 @@
 import math
+import json
 import pandas as pd
 import dash_core_components as dcc
 import dash_html_components as html
+import dash_bootstrap_components as dbc
 from dash.dependencies import Input, Output
 
 from app import app
 
-artist_df = pd.read_json("data/artist_graph.json", orient="index")
+with open(f"data/lyric.json", "r") as f:
+    all_df = pd.DataFrame.from_dict(json.load(f), orient="index")
 
 
-def get_figure(lang, size, color):
-    all_df = artist_df[artist_df["lang"] == lang]
-
-    colors = ["#fc8d62", "#8da0cb", "#66c2a5"]
-    traces = []
-    edge_x = []
-    edge_y = []
-    for _, row in all_df.iterrows():
-        x0, y0 = row["pos"]
-        x1, y1 = row["pos_sim_1"]
-        edge_x += [x0, x1, None]
-        edge_y += [y0, y1, None]
-    traces.append({
-        "name": "Most Similar With",
-        "x": edge_x,
-        "y": edge_y,
-        "mode": "lines",
-        "hoverinfo": "none",
-        "line": {
-            "width": 0.5,
-            "color": "DarkSlateGrey",
-            "shape": "spline"
-        }
-    })
-    if color == "era":
-        color_cats = ["oldies", "90s", "2000s"]
-        labels = ["Before 1990", "1990-2000", "After 2000"]
+def get_figure(lang, genres, bill_year, search_input, search_dropdown):
+    temp_lang = lang if lang else "en"
+    lyric_df = all_df[(all_df["bill_year"] == bill_year) & all_df.index.str.contains(lang)].copy()
+    grouped_genre = lyric_df["main_genre"].apply(
+        lambda genre: genre if genre in ["rock", "pop"] else "other"
+    )
+    lyric_df = lyric_df[grouped_genre.isin(genres)]
+    if len(search_input) > 0:
+        mask = lyric_df[search_dropdown].str.contains(search_input) if search_dropdown and search_dropdown != "all" \
+            else lyric_df["artists"].str.contains(search_input) | lyric_df["title"].str.contains(search_input)
     else:
-        color_cats = ["rock", "pop", "other"]
-        labels = ["Rock", "Pop", "Other Genres"]
-    for i, c in enumerate(color_cats):
-        artist_graph_df = all_df[all_df[color] == c] if c != "other" else all_df[~all_df[color].isin(color_cats[:-1])]
-        node_x = [pos[0] for pos in artist_graph_df["pos"]]
-        node_y = [pos[1] for pos in artist_graph_df["pos"]]
-        hover_remark = {
-            "node_adjacencies": "connection",
-            "song_count": "song"
-        }
-        custom_data = [[
-            r["artist"],
-            r[size],
-            hover_remark[size] + ("s" if r[size] > 1 else "")
-        ] for _, r in artist_graph_df.iterrows()]
-        node_text = artist_graph_df["artist"]
-        node_size = [(s // 6 + 1) * 6 for s in artist_graph_df[size]]
-        traces.append({
-            "name": labels[i],
-            "x": node_x,
-            "y": node_y,
-            "mode": "markers",
-            "text": node_text,
-            "customdata": custom_data,
-            "hovertemplate": "<b>%{customdata[0]}</b><br>%{customdata[1]} %{customdata[2]}",
-            "marker": {
-                "symbol": "circle",
-                "size": node_size,
-                "opacity": 1,
-                "color": colors[i],
-                "line": {
-                    "width": 1,
-                    "color": "DarkSlateGrey"
-                }
-            }
-        })
-    figure = {
-            "data": traces,
-            "layout": {
-                "hovermode": "closest",
-                # "width": 800,
-                "height": 600,
-                "xaxis": {
-                    "showgrid": False,
-                    "zeroline": False,
-                    "showticklabels": False
+        mask = True
+    colors = ["#8da0cb", "#66c2a5", "#fc8d62"]
+    traces = []
+
+    for m in [False, True]:
+        for i, state in enumerate(["oldies", "90s", "2000s"]):
+            trace_df = lyric_df[(lyric_df["era"] == state) & mask] if m else lyric_df[(lyric_df["era"] == state) & ~mask]
+            custom_data = [(r["title"], r["artists"], r["bill_rank"]) for _, r in trace_df.iterrows()]
+            trace = {
+                "name": state,
+                "x": trace_df["pca1"],
+                "y": trace_df["pca2"],
+                "mode": "markers",
+                "text": trace_df["song_id"],
+                "customdata": custom_data,
+                "showlegend": m,
+                "marker": {
+                    "symbol": "circle",
+                    "size": 10,
+                    "opacity": 0.8 if m else 0.1,
+                    "color": colors[i] if m else "#666666",
+                    # "line": {
+                    #     "width": 1,
+                    #     "color": "DarkSlateGrey"
+                    # }
                 },
-                "yaxis": {
-                    "showgrid": False,
-                    "zeroline": False,
-                    "showticklabels": False
-                }
+            }
+            if m:
+                trace["hovertemplate"] = "<b>%{customdata[0]}</b><br>%{customdata[1]}<br>Rank=%{customdata[2]}"
+            else:
+                trace["hoverinfo"] = "skip"
+            traces.append(trace)
+
+    figure = {
+        "data": traces,
+        "layout": {
+            "hovermode": "closest",
+            "xaxis": {
+                # "showgrid": False,
+                "zeroline": False,
+                # "showticklabels": False
+            },
+            "yaxis": {
+                # "showgrid": False,
+                "zeroline": False,
+                # "showticklabels": False
+            },
+            "legend": {
+                "x": 0.5,
+                "y": -0.2,
+                "xanchor": "center",
+                "yanchor": "top",
+                "orientation": "h"
             }
         }
+    }
     return figure
 
 
-def get_artist_table(img_src, name, avg_song_year, genre, num_songs, similar_artists):
+def get_artist_table(title, artists, first_artist_img, genre, song_year, debut_year, hp, hp_years, similar_songs):
     table_components = [
-        html.Tr([html.Td(html.Img(src=img_src, height=120), colSpan=2, style={"text-align": "center"})]),
-        html.Tr([html.Th(name, colSpan=2, style={"text-align": "center"})]),
-        html.Tr([html.Td("Active Around", style={"text-align": "right"}), html.Td(avg_song_year)]),
-        html.Tr([html.Td("Main Genre", style={"text-align": "right"}), html.Td(genre)]),
-        html.Tr([html.Td("#Songs in Top-200", style={"text-align": "right"}), html.Td(num_songs)]),
-        html.Tr([html.Th("Similar Artists", colSpan=2, style={"text-align": "center"})])
+        html.Tr([html.Th(title, colSpan=2, style={"text-align": "center"})]),
+        html.Tr([html.Td(f"by {artists}", colSpan=2, style={"text-align": "center"})]),
+        html.Tr([html.Td(html.Img(src=first_artist_img, height=120), colSpan=2, style={"text-align": "center"})]),
+        html.Tr([html.Td("Released", style={"text-align": "right"}), html.Td(song_year)]),
+        html.Tr([html.Td("Genre", style={"text-align": "right"}), html.Td(genre)]),
+        html.Tr([html.Td("First Year in Chart", style={"text-align": "right"}), html.Td(debut_year)]),
+        html.Tr([html.Td("Highest Position", style={"text-align": "right"}), html.Td(f"{hp} {hp_years}")]),
+        html.Tr([html.Th("The lyric is similar to...", colSpan=2, style={"text-align": "center"})])
     ]
-    for similar_artist in similar_artists:
-        table_components.append(
+    for similar_song in similar_songs:
+        table_components += [
             html.Tr([
-                html.Td(html.Img(src=similar_artist[1], height=80), style={"text-align": "right"}),
-                html.Td(similar_artist[0])
+                html.Td(html.Img(src=similar_song[0], height=80), style={"text-align": "right"}, rowSpan=2),
+                html.Td(f"{similar_song[1]} ({similar_song[2]}%)")
+            ]),
+            html.Tr([
+                html.Td(f"by {similar_song[3]}")
             ])
-        )
-    return html.Table(table_components)
+        ]
+    return dbc.Table(table_components)
 
 
-content = html.Div([
-    html.Div(children=[
-        html.H1(children="The Artist Constellation"),
-        html.H4(children="Based on lyrics similarity"),
-        html.Div(children=[
-            html.Div(children="Language:", style={"width": "30%", "display": "inline-block"}),
-            html.Div(children="Size:", style={"width": "30%", "display": "inline-block"}),
-            html.Div(children="Colors:", style={"width": "30%", "display": "inline-block"})
-        ]),
-        html.Div(children=[
-            html.Div(children=[
+body_container = [
+    dbc.Row([
+        dbc.Col([
+            dbc.FormGroup([
+                dbc.Label("Language"),
                 dcc.Dropdown(
-                    id="language",
+                    id="language-dropdown",
                     options=[{"label": "English", "value": "en"}, {"label": "Dutch", "value": "nl"}],
                     value="en"
                 )
-            ],
-                style={"width": "30%", "display": "inline-block", "padding": "10px none"}
-            ),
-            html.Div(children=[
-                dcc.Dropdown(
-                    id="size",
+            ])
+        ], width=4),
+        dbc.Col([
+            dbc.FormGroup([
+                dbc.Label("Genre"),
+                dcc.Checklist(
+                    id="genre-checklist",
                     options=[
-                        {"label": "#Connections", "value": "node_adjacencies"},
-                        {"label": "#Songs in Top-200", "value": "song_count"}
+                        {"label": "Rock", "value": "rock"},
+                        {"label": "Pop", "value": "pop"},
+                        {"label": "Other", "value": "other"},
                     ],
-                    value="node_adjacencies"
+                    value=["rock", "pop", "other"],
                 )
-            ],
-                style={"width": "30%", "display": "inline-block", "padding": "10px none"}
-            ),
-            html.Div(children=[
+            ])
+        ], width=4),
+        dbc.Col([
+            dbc.FormGroup([
+                dbc.Label("Search"),
                 dcc.Dropdown(
-                    id="color",
+                    id="search-dropdown",
                     options=[
-                        {"label": "Era", "value": "era"},
-                        {"label": "Genre", "value": "main_genre"}
+                        {"label": "All", "value": "all"},
+                        {"label": "Artists", "value": "artists"},
+                        {"label": "Titles", "value": "title"},
                     ],
-                    value="era"
+                    value="all"
+                ),
+                dcc.Input(
+                    id="search-input",
+                    value="",
+                    type="text"
                 )
-            ],
-                style={"width": "30%", "display": "inline-block", "padding": "10px none"}
-            )
-        ]),
-        html.Div(children=[
-            dcc.Graph(
-                id="fig",
-                figure=get_figure("en", "node_adjacencies", "era")
-            )
-        ]),
-    ],
-        style={"width": "70%", "display": "inline-block"}
+            ])
+        ], width=4)
+    ]),
+    dcc.Graph(
+        id="fig",
+        figure=get_figure("en", ["rock", "pop", "other"], 1999, "", "all")
     ),
-    html.Div(
-        id="artist_table",
-        children=[],
-        style={"width": "20%", "display": "inline-block", "vertical-align": "top", "padding": "10px"}
+    dcc.Slider(
+        id='ranking-year-slider',
+        min=1999,
+        max=2019,
+        value=1999,
+        marks={str(year): str(year) for year in range(1999, 2020)},
+        step=None
     )
-])
+]
+
+
+description = html.Div(id="detail", children="Description")
+
+
+content = dbc.Container([
+    dbc.Row([
+        dbc.Col(body_container, width=8),
+        dbc.Col([
+            dbc.Row(children=description)
+        ], width=4)
+    ])
+], fluid=True)
 
 
 @app.callback(
     Output("fig", "figure"),
-    [Input("language", "value"), Input("size", "value"), Input("color", "value")]
+    [
+        Input("language-dropdown", "value"),
+        Input("genre-checklist", "value"),
+        Input("ranking-year-slider", "value"),
+        Input("search-input", "value"),
+        Input("search-dropdown", "value")
+    ]
 )
-def display_figure(lang, size, color):
-    return get_figure(lang, size, color)
+def display_figure(lang, genres, ranking_year, search_input, search_dropdown):
+    return get_figure(lang, genres, ranking_year, search_input, search_dropdown)
 
 
 @app.callback(
-    Output("artist_table", "children"),
+    Output("detail", "children"),
     [
-        Input("language", "value"),
+        Input("language-dropdown", "value"),
         Input("fig", "clickData")
     ]
 )
 def display_artist_info(lang, click_data):
+    temp_lang = lang if lang else "en"
     if click_data is None or "text" not in click_data["points"][0]:
-        return [html.Div("Click an artist...")]
-    curr_artist = click_data["points"][0]["text"]
-    attention_artist = artist_df[
-        (artist_df["lang"] == lang) &
-        (artist_df["artist"] == curr_artist)
-        ]
-    if len(attention_artist) == 0:
-        return [html.Div("Click an artist...")]
-    artist = attention_artist.iloc[0]
-    genre = artist["main_genre"]
-    num_songs = artist["song_count"]
-    avg_song_year = math.ceil(artist["avg_song_year"])
-    curr_img = artist["images"][1]["url"]
-    similar_artists = []
-    i = 1
-    while len(similar_artists) < 3 and i < 6:
-        similar_artist_name = artist[f"similar_artist_{i}"]
-        similar_artist = artist_df[artist_df["artist"] == similar_artist_name]
-        if len(similar_artist) > 0:
-            similar_img = similar_artist.iloc[0]["images"][1]["url"]
-            similar_artists.append((similar_artist_name, similar_img))
-        i += 1
-    return get_artist_table(curr_img, curr_artist, avg_song_year, genre, num_songs, similar_artists)
+        return [html.Div("Click an artist")]
+    with open("data/artist_images.json", "r") as f_in:
+        img_dict = json.load(f_in)
+    curr_song_id = click_data["points"][0]["text"]
+    song_data = all_df[all_df["song_id"] == curr_song_id].iloc[0]
+    artists = song_data["artists"]
+    title = song_data["title"]
+    genre = song_data["main_genre"]
+    hp = song_data["hp"]
+    song_year = song_data["year"]
+    debut_year = song_data["debut_year"]
+    hp_years = song_data["hp_years"]
+    first_artist = song_data["first_artist"]
+    first_artist_img = img_dict[first_artist]["images"][1]["url"] if first_artist in img_dict else None
+    similar_songs = []
+    for sim in song_data["similar"]:
+        sim_title = sim["title"]
+        sim_artists = sim["all_artists"]
+        sim_prob = "%.2f" % (sim["similarity"] * 100)
+        sim_first_artist = sim["first_artist"]
+        sim_first_artist_img = img_dict[sim_first_artist]["images"][1]["url"] if sim_first_artist in img_dict else None
+        similar_songs.append((
+            sim_first_artist_img,
+            sim_title,
+            sim_prob,
+            sim_artists
+        ))
+    return get_artist_table(title, artists, first_artist_img, genre, song_year, debut_year, hp, hp_years, similar_songs)
