2c146b665bc9374384c51d801550a664e39f1071
Nemania Borovits <nemaniaborovits@gmail.com>
2020-05-05 13:33:32 +0200

    feature fix attempt 3

--- utils.py
+++ utils.py
@@ -32,19 +32,19 @@ def create_era_df(df):
         first_title_df = best_era_df[["index", "title"]].groupby("title").rank(method="first")
         best_era_df = best_era_df[first_title_df["index"] == 1]
 
-        best_era_df = best_era_df[[
+        best_era_df2 = best_era_df[[
             'era', 'duration_ms', 'followers',
        'analysis_loudness', 'analysis_tempo', 'feature_danceability',
        'feature_energy', 'feature_speechiness', 'feature_instrumentalness',
        'feature_liveness', 'feature_valence'
-        ]]
+        ]].copy()
         
-        best_era_df = best_era_df.groupby("era").mean().reset_index().melt(id_vars=["era"])
-        compare_oldies_df = best_era_df[best_era_df["era"] == "oldies"][["variable", "value"]].rename(columns={"value": "value_oldies"})
-        best_era_df = best_era_df.merge(compare_oldies_df, how="left", on="variable")
-        best_era_df["relative_to_oldies"] = (best_era_df["value"] - best_era_df["value_oldies"]) * 100 / best_era_df["value_oldies"]
-        best_era_df["era"] = best_era_df["era"].map({"oldies": "1920-1980", "90s": "1980-1990", "2000s": "1990-2020"})
-        best_era_df = best_era_df[["era", "variable", "relative_to_oldies"]].sort_values(["era", "relative_to_oldies"])
-        best_era_df.columns = ["Song Era", "Song Features", "Compared to Oldies (Song Released < 1990)"]
+        best_era_df2 = best_era_df2.groupby("era").mean().reset_index().melt(id_vars=["era"])
+        compare_oldies_df = best_era_df2[best_era_df2["era"] == "oldies"][["variable", "value"]].rename(columns={"value": "value_oldies"})
+        best_era_df2 = best_era_df2.merge(compare_oldies_df, how="left", on="variable")
+        best_era_df2["relative_to_oldies"] = (best_era_df2["value"] - best_era_df2["value_oldies"]) * 100 / best_era_df2["value_oldies"]
+        best_era_df2["era"] = best_era_df2["era"].map({"oldies": "1920-1980", "90s": "1980-1990", "2000s": "1990-2020"})
+        best_era_df2 = best_era_df2[["era", "variable", "relative_to_oldies"]].sort_values(["era", "relative_to_oldies"])
+        best_era_df2.columns = ["Song Era", "Song Features", "Compared to Oldies (Song Released < 1990)"]
 
-        return best_era_df
+        return best_era_df2
