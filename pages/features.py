98bec80f80c19305874c5b554b6aaec9599954c7
Nemania Borovits <nemaniaborovits@gmail.com>
2020-05-05 12:51:27 +0200

    feature filters, remove file from data, update read_me

--- pages/features.py
+++ pages/features.py
@@ -8,13 +8,21 @@ import pickle
 
 from dash.dependencies import Output, Input
 
-from utils import generate_table
+from utils import generate_table,create_era_df
 from app import app
 
 # DATA LOADING
 
-with open('data/best_era_df.pkl', 'rb') as input_file:
-    best_era_df = pickle.load(input_file)
+# with open('data/best_era_df.pkl', 'rb') as input_file:
+#     best_era_df = pickle.load(input_file)
+
+bill_join_df = pd.read_csv("data/bill_join_df.csv", index_col=0)
+bill_join_df = bill_join_df.dropna(subset=['main_genre','is_dutch'])
+
+genres = bill_join_df['main_genre'].unique().tolist()
+# genres[1] = 'other'
+bill_join_df.is_dutch = bill_join_df.is_dutch.map({True:'Dutch', False:'Non-Dutch'})
+origin_list = bill_join_df.is_dutch.unique().tolist()
 
 feature_list = ['Danceability','Energy','Speachiness','Instrumentalness','Liveness','Valence','Tempo']
 
@@ -35,68 +43,44 @@ features_descriptions = features_descriptions.rename(columns={0:'Feature descrip
 
 color_sequence=["#bdbdbd", "#9ecae1", "#3182bd"]
 
-fig_dict = {
-    'data':[
-        dict(
-            x=best_era_df[best_era_df['Song Era'] == i]['Compared to Oldies (Song Released < 1990)'],
-            y=best_era_df[best_era_df['Song Era'] == i]['Song Features'],
-            mode='markers',
-            marker=dict(
-                line_width=1, 
-                symbol='circle', 
-                size=16,
-                color= color_sequence[np.where(best_era_df['Song Era'].unique() == i)[0][0]]
-            ),
-            name = i
-        ) for i in best_era_df['Song Era'].unique()
-    ],
-    # 'template':'plotly_white',
-    'layout':dict(
-        title='Comparing Features of Different Song Eras (based on Spotify API)',                                xaxis=dict(
-            title='Compared to Oldies (Song Released < 1990)',
-            showgrid=False,
-            showline=True,
-            linecolor='rgb(102, 102, 102)',
-            tickfont_color='rgb(102, 102, 102)',
-            showticklabels=True,
-            dtick=25,
-            ticks='outside',
-            tickcolor='rgb(102, 102, 102)',
-            ticksuffix='%'
-        ),
-        yaxis=dict(title='Song features'),
-        margin=dict(l=140, r=0, b=50, t=80),
-        legend=dict(
-            font_size=10,
-            yanchor='middle',
-            xanchor='right',
-        ),
-        paper_bgcolor='white',
-        plot_bgcolor='white',
-        hovermode='closest',
-        width=1024,
-        height=760,
-    )
-}
-fig = go.Figure(fig_dict)
 # CONTENT
 content = html.Div(
     [
         dbc.Row(
             [
+                dbc.Col(html.Div([
+                    html.H3("Comparing Features of Different Song Eras (based on Spotify API)")
+                ]),className="text-center",),
+            ]
+        ),
+        dbc.Row(
+            [
                 # dbc.Col(html.Div([
-                #     dcc.Dropdown(
-                #         id='oldest-first',
-                #         options=[{'label': key, 'value': key} for key in feature_list],
-                #         value= 'Danceability'
-                #     ),
-                # ]),width=2,align="center"),
-                
+                #     html.P("Filters")
+                # ]),width={"size":1,"order":1,"offset": 1}),
+                dbc.Col(html.Div([
+                    dcc.Dropdown(
+                        id='genres',
+                        options=[{'label': key, 'value': key} for key in genres],
+                        value= 'rock'
+                    ),
+                ]),width={"size":3,"order":1,"offset": 2}),
+                dbc.Col(html.Div([
+                    dcc.Dropdown(
+                        id='dutch',
+                        options=[{'label': key, 'value': key} for key in origin_list],
+                        value= 'Dutch'
+                    ),
+                ]),width={"size":3,"order":2,"offset": 12}),
+            ],
+        style={'margin-top':'20px'}),
+        dbc.Row(
+            [
                 dbc.Col(html.Div(children=[
                     dcc.Graph(
                         id='song-feature-99',
                         config=dict(responsive=True),
-                        figure = fig
+                        # figure = fig
                     ),
                 ]),width="auto"),
                 dbc.Col(generate_table(features_descriptions, max_rows=8),width="auto",align="center"),
@@ -109,4 +93,69 @@ content = html.Div(
 )
 
 
-# CALLBACKS
+#CALLBACKS
+
+#genre dropdown
+@app.callback(
+    Output('song-feature-99', 'figure'),
+    [
+        Input('genres', 'value'),
+        Input('dutch', 'value')
+    ])
+def update_figure_genre(selected_genre, selected_origin,):
+    
+    filtered_genre = bill_join_df[bill_join_df['main_genre'] == selected_genre]
+    filtered_genre = filtered_genre[filtered_genre['is_dutch'] == selected_origin]
+
+    best_era_df = create_era_df(filtered_genre)
+
+    traces = []
+    for i in best_era_df['Song Era'].unique():
+        df = best_era_df[best_era_df['Song Era'] == i]
+        traces.append(dict(
+            x=df['Compared to Oldies (Song Released < 1990)'],
+            y=df['Song Features'],
+            # text= df["title"],
+            mode='markers',
+            # color=,
+            opacity=0.7,
+            marker=dict(
+                line_width=1, 
+                symbol='circle', 
+                size=16,
+                color= color_sequence[np.where(best_era_df['Song Era'].unique() == i)[0][0]]
+            ),
+            name=i
+        ))
+
+    return {
+        'data': traces,
+        'layout': dict(
+            # title = 'Comparing Features of Different Song Eras (based on Spotify API)',
+            xaxis=dict(
+                title = '',
+                showgrid=False,
+                showline=True,
+                linecolor='rgb(102, 102, 102)',
+                tickfont_color='rgb(102, 102, 102)',
+                showticklabels=True,
+                dtick=25,
+                ticks='outside',
+                tickcolor='rgb(102, 102, 102)',
+                ticksuffix='%'
+            ),
+            yaxis=dict(title='Song features'),
+            margin=dict(l=140, r=0, b=50, t=80),
+            legend=dict(
+                font_size=10,
+                yanchor='middle',
+                xanchor='right',
+            ),
+            paper_bgcolor='white',
+            plot_bgcolor='white',
+            hovermode='closest',
+            width=1024,
+            height=760,
+        )
+    }
+
--- utils.py
+++ utils.py
@@ -2,6 +2,9 @@ import dash_html_components as html
 
 
 def generate_table(dataframe, max_rows):
+
+    """ A function which returns a responsive html table provided a dataframe """
+
     return html.Div(children=[
         html.Table([
             html.Thead(
@@ -14,3 +17,34 @@ def generate_table(dataframe, max_rows):
             ])
         ], className="table-bordered")
     ], className="table-responsive")
+
+def create_era_df(df):
+
+        song_features = [
+            'duration_ms', 'followers', 'analysis_loudness', 'analysis_tempo',
+            'feature_danceability', 'feature_energy', 'feature_speechiness', 'feature_instrumentalness', 
+            'feature_liveness', 'feature_valence'
+        ]
+
+        era_df = df.groupby(["title", "era", "hp"]).count().reset_index()[["title", "era", "hp", "artist"]].rename(columns={"artist": "count"})
+        era_df["era_rank"] = era_df.groupby("era").rank(method="first")["hp"]
+        best_era_df = df[df["title"].isin(era_df[era_df["era_rank"] < 200]["title"].unique())].copy()[["title", "era"] + song_features].dropna().reset_index()
+        first_title_df = best_era_df[["index", "title"]].groupby("title").rank(method="first")
+        best_era_df = best_era_df[first_title_df["index"] == 1]
+        del best_era_df["index"]
+        del best_era_df["title"]
+        best_era_df.columns = [
+            "era", "Duration", "Mainstream", "Loudness", "Tempo",
+            "Danceability", "Energy", "Speechiness", "Instrumentalness",
+            "Liveness", "Valence"
+        ]
+        # all_cols = list(best_era_df.columns.values)
+        best_era_df = best_era_df.groupby("era").mean().reset_index().melt(id_vars=["era"])
+        compare_oldies_df = best_era_df[best_era_df["era"] == "oldies"][["variable", "value"]].rename(columns={"value": "value_oldies"})
+        best_era_df = best_era_df.merge(compare_oldies_df, how="left", on="variable")
+        best_era_df["relative_to_oldies"] = (best_era_df["value"] - best_era_df["value_oldies"]) * 100 / best_era_df["value_oldies"]
+        best_era_df["era"] = best_era_df["era"].map({"oldies": "1920-1980", "90s": "1980-1990", "2000s": "1990-2020"})
+        best_era_df = best_era_df[["era", "variable", "relative_to_oldies"]].sort_values(["era", "relative_to_oldies"])
+        best_era_df.columns = ["Song Era", "Song Features", "Compared to Oldies (Song Released < 1990)"]
+
+        return best_era_df
