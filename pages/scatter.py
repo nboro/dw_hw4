77dbf7d90ed07ef231c9387941923344d091823b
Chris <xristossogr@gmail.com>
2020-05-05 12:13:28 +0200

    Update scatter.py
    Update Scatter (changing filters)

--- pages/scatter.py
+++ pages/scatter.py
@@ -11,82 +11,66 @@ top_df = bill_join_df[bill_join_df["bill_rank"] <= 200][["artist", "title", "yea
 top_df["bill_year_w_jitter"] = top_df["bill_year"].apply(lambda y: y + np.random.normal(0, 0.1))
 
 year_chart_df = top_df.rename(columns={"year": "Song Release Year", "bill_year_w_jitter": "Ranking Year", "is_dutch": "Artist Country"})
-# year_chart_df["main_genre"] = year_chart_df["main_genre"].apply(lambda c: "Pop" if c == 'pop' else "Other")
+year_chart_df['main_genre'] = year_chart_df['main_genre'].map({'rock': 'rock', 'pop': 'pop','reggae':'other','soul':'other','folk':'other','hip hop':'other','metal':'other','house':'other','disco':'other','others':'other'})
 genre_list = year_chart_df["main_genre"].unique().tolist()
 genre_list.append("All")
 year_chart_df['Artist Country'] = year_chart_df['Artist Country'].map({True: 'Dutch', False: 'Non Dutch'})
-
-
-
-#
-# fig = px.scatter(
-#                     year_chart_df,
-#                     x="Ranking Year",
-#                     y="Song Release Year",
-#                     color="main_genre",
-#                     template="simple_white",
-#                     labels=year_chart_df.apply(lambda row: "f{row['artist']}, {row['title']} ({row['Song Release Year']})", axis=1),
-#                     hover_name="title",
-#                     hover_data=["artist"],
-#                     title="Top 200 NPO Ranking by Release Year"
-# )
-# _ = fig.update_traces(opacity=0.6, marker={"line": {"width": 1, "color": "DarkSlateGrey"}})
-# _ = fig.update_layout(
-#     width=1200,
-#     height=760
-# )
+isDutch_list = year_chart_df['Artist Country'].unique().tolist()
+isDutch_list.append("All")
 
 
 
 content  = html.Div([
-    dcc.Graph(id='graph-with-slider'),
+    dcc.Graph(id='graph-with-slider',style={'width': 1600}),
     dcc.Dropdown(
-        id='genre-dropdown',
-        options = [{'label': key, 'value': key} for key in genre_list ],
+        id='isDutch-dropdown',
+        options = [{'label': key, 'value': key} for key in isDutch_list ],
         value = 'All'
     ),
-    # dcc.Slider(
+    # dcc.RangeSlider(
     #     id='year-slider',
-    #     min=year_chart_df.head(10)
-    #     max=year_chart_df.head(200)
-    #     value=df['year'].min(),
-    #     marks={str(year): str(year) for year in df['year'].unique()},
-    #     step=None
-    # )
+    #     updatemode = 'mouseup',
+    #     min=genre_list[0],
+    #     max=genre_list[199],
+    #     value=[genre_list[0],genre_list[199]],
+    #     marks={str(year): str(year) for year in genre_list},
+    #
+    # ),
 ])
 
 
 @app.callback(
     Output('graph-with-slider', 'figure'),
-    [Input('genre-dropdown', 'value')])
-def update_figure(selected_genre):
+    [Input('isDutch-dropdown', 'value')])
+def update_figure(selected_isDutch):
     if selected_genre != "All":
-        filtered_year_chart_df = year_chart_df[year_chart_df.main_genre == selected_genre]
+        filtered_year_chart_df = year_chart_df[year_chart_df['Artist Country'] == selected_isDutch]
     else:
         filtered_year_chart_df = year_chart_df
     traces = []
-    for i in filtered_year_chart_df['Artist Country'].unique():
-        df = filtered_year_chart_df[filtered_year_chart_df['Artist Country'] == i]
+    for i in filtered_year_chart_df['main_genre'].unique():
+        df = filtered_year_chart_df[filtered_year_chart_df['main_genre'] == i]
         traces.append(dict(
             x=df["Ranking Year"],
             y=df["Song Release Year"],
             text= df["title"],
             mode='markers',
-            # color=,
             opacity=0.7,
             marker={
-                'size': 15,
+                'size': 8,
                 'line': {'width': 0.5, 'color': 'white'}
             },
             name=i
         ))
 
+
+
     return {
         'data': traces,
         'layout': dict(
-            xaxis={'title': 'This is x title',
+            xaxis={'title': 'Ranking Year',
                    'range':[1998, 2020]},
-            yaxis={'title': 'This is y title', 'range': [1950, 2020]},
+            yaxis={'title': 'Song Release Year', 'range': [1955, 2020]},
             margin={'l': 40, 'b': 40, 't': 10, 'r': 10},
             legend={'x': 0, 'y': 1},
             hovermode='closest',
