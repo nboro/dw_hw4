424a2bbc49191162aa12dfeb64c4ec507889d314
Chris <xristossogr@gmail.com>
2020-05-09 00:19:47 +0200

    Description added, hovering fixing

--- pages/scatter.py
+++ pages/scatter.py
@@ -4,15 +4,17 @@ import dash_core_components as dcc
 import dash_bootstrap_components as dbc
 import dash_html_components as html
 from dash.dependencies import Input, Output
-from utils import get_graph_template
+from utils import get_graph_template, get_song_card
 from app import app
 
 bill_join_df = pd.read_csv("data/bill_join_df.csv", index_col=0)
 
-top_df = bill_join_df[bill_join_df["bill_rank"] <= 200][["artist", "title", "year", "is_dutch", "main_genre", "bill_rank", "bill_year"]].dropna()
+top_df = bill_join_df[bill_join_df["bill_rank"] <= 200][
+    ["artist", "title", "year", "is_dutch", "main_genre", "bill_rank", "bill_year"]].dropna()
 top_df["bill_year_w_jitter"] = top_df["bill_year"].apply(lambda y: y + np.random.normal(0, 0.1))
 
-year_chart_df = top_df.rename(columns={"year": "Song Release Year", "bill_year_w_jitter": "Ranking Year", "is_dutch": "Artist Country"})
+year_chart_df = top_df.rename(
+    columns={"year": "Song Release Year", "bill_year_w_jitter": "Ranking Year", "is_dutch": "Artist Country"})
 year_chart_df['main_genre'] = year_chart_df['main_genre'].apply(lambda g: g if g in ["rock", "pop"] else "others")
 genre_list = year_chart_df["main_genre"].unique().tolist()
 genre_list.append("All")
@@ -57,134 +59,18 @@ content = [
         dbc.Col([
             dbc.Label(children="Top-n:"),
             dcc.Slider(
-                    id='rank-slider',
-                    min=10,
-                    max=200,
-                    value=200,
-                    marks={str(pos): str(pos) for pos in range(10,201,10)},
-                    step=10,
+                id='rank-slider',
+                min=10,
+                max=200,
+                value=200,
+                marks={str(pos): str(pos) for pos in range(10, 201, 10)},
+                step=10,
             ),
         ])
     ])
 ]
 
-description = html.Div(id="scatter-description", children="")
-
-
-# content = html.Div([
-#
-#     dbc.Row([
-#         dbc.Col(html.Div([
-#             html.H2("Release year of top-200 songs")
-#         ]),className="text-center"),
-#     ]),
-#
-#     dbc.Row([
-#
-#                 dbc.Col(html.Div([
-#                     dcc.Graph(id='graph-with-slider'),
-#
-#                     dcc.Slider(
-#                             id='rank-slider',
-#                             min=10,
-#                             max=200,
-#                             value=200,
-#                             marks={str(pos): str(pos) for pos in range(10,201,10)},
-#                             step=10,
-#
-#                     ),
-#
-#                 ])),
-#
-#
-#                 dbc.Col(html.Div([
-#                     dcc.Dropdown(
-#                         id='isDutch-dropdown',
-#                         options=[{'label': key, 'value': key} for key in isDutch_list],
-#                         value='All')
-#                 ]),width={"size":3,"order":2,"offset": 0}),
-#     ]),
-#
-#
-#     dbc.Row([
-#
-#                 dbc.Col(html.Div([
-#                     dcc.Graph(id='graph-with-slider'),
-#
-#                     dbc.Row([
-#                         dbc.Col(html.Div([
-#                             html.H6("Filter by top k songs")
-#                         ]), className="text-center", ),
-#
-#
-#                     ]),
-#
-#                     dcc.Slider(
-#                             id='rank-slider',
-#                             min=10,
-#                             max=200,
-#                             value=200,
-#                             marks={str(pos): str(pos) for pos in range(10,201,10)},
-#                             step=10,
-#
-#                     ),
-#
-#                 ])),
-#
-#
-#                 dbc.Col(html.Div([
-#                     dcc.Dropdown(
-#                         id='isDutch-dropdown',
-#                         options=[{'label': key, 'value': key} for key in isDutch_list],
-#                         value='All')
-#                 ]),width={"size":3,"order":2,"offset": 0}),
-#     ]),
-#
-#     # dbc.Row(
-#     #     [
-#     #         dbc.Col(html.Div(children=[
-#     #                     dcc.Slider(
-#     #                         id='rank-slider',
-#     #                         min=10,
-#     #                         max=200,
-#     #                         value=200,
-#     #                         marks={str(pos): str(pos) for pos in range(10,211,20)},
-#     #                         step=10,
-#     #
-#     #                     ),
-#     #         ])),
-#     #         dbc.Col(id='feature_text', width="auto", align="center")
-#     #     ],
-#     #     justify="center",
-#     # )
-#     ],
-#     style={'margin-top':'20px'}),
-
-
-    # html.Div(
-    #     [
-    #         dcc.Graph(id='graph-with-slider',style={'width': '70%'}),
-    #
-    #         dcc.Dropdown(
-    #             id='isDutch-dropdown',
-    #             options=[{'label': key, 'value': key} for key in isDutch_list],
-    #             value='All'
-    #         ),
-    #
-    #         dcc.Slider(
-    #             id='rank-slider',
-    #             min=10,
-    #             max=200,
-    #             value=200,
-    #             marks={str(pos): str(pos) for pos in range(10,211,20)},
-    #             step=10,
-    #
-    #         ),
-    #
-    #     ],
-    #     style={'width': '100%',
-    #            'display': 'inline-block'}),
-# ])
+description = html.Div(id='scatter-description', children="Click a dot..")
 
 
 @app.callback(
@@ -203,12 +89,20 @@ def update_figure(selected_isDutch, bill_rank_lista):
     traces = []
     for i in filtered_year_chart_df['main_genre'].unique():
         df = filtered_year_chart_df[filtered_year_chart_df['main_genre'] == i]
+        custom_data = [(r["title"], r["bill_rank"], idx) for idx, r in
+                       filtered_year_chart_df.iterrows()]
+
+        # ranking_year = str(df.iloc[0]["Ranking Year"])
+        # bill_rank = df[0]['bill_rank']
+
         traces.append(dict(
             type="scattergl",
             x=df["Ranking Year"],
             y=df["Song Release Year"],
             text= df["title"],
+            hoverinfo = 'text',
             mode='markers',
+            customdata=custom_data,
             opacity=0.7,
             marker={
                 'size': 8,
@@ -216,8 +110,6 @@ def update_figure(selected_isDutch, bill_rank_lista):
             },
             name=i
         ))
-
-
 
     return {
         'data': traces,
@@ -231,3 +123,32 @@ def update_figure(selected_isDutch, bill_rank_lista):
         #     hovermode='closest',
         # )
     }
+
+
+@app.callback(Output("scatter-description", "children"), [Input("graph-with-slider", "clickData")])
+def display_artist(clickData):
+    if clickData:
+        click = clickData['points'][0]
+
+        titlos = click['customdata'][0]
+        rankyear = round(click['x'])
+        billrank = click['customdata'][1]
+
+        # table_header = [html.Thead(html.Tr([html.Th("titlos"), html.Th("rankyear"), html.Th("billrank")]))]
+        row1 = html.Tr([html.Td(dcc.Markdown('''**Title is**''')), html.Td(titlos)])
+        row2 = html.Tr([html.Td(dcc.Markdown('''**Rank Year**''')), html.Td(rankyear)])
+        row3 = html.Tr([html.Td(dcc.Markdown('''**Bill Rank**''')), html.Td(billrank)])
+        # max_key = feature_map[feature]
+        # max_value = features_max[max_key]
+        # title, artist, genre = max_value.split(sep='_')
+        # row2 = html.Tr([html.Td(dcc.Markdown('''
+        #             The song with the highest *''' + feature + '''* is **''' + title + '''** performed by **''' + artist + '''** and belongs to the **''' + genre + '''** genre.'''
+        #                                      ))])
+
+        table_body = [html.Tbody([row1,row2,row3])]
+        # table_body2 = [html.Tbody([row2])]
+        table = dbc.Table(table_body)
+        # table2 = dbc.Table(table_body2, bordered=False, hover=True, responsive=True)
+        return html.Div(children=[
+            table
+        ], className="table-responsive")
