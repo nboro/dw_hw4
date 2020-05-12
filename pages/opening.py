2ebb45da5daddfb3e5aaf620a31180ed63b838cd
Hameez Ariz <MHameez@users.noreply.github.com>
2020-05-12 12:02:12 +0200

    Intro updated

--- pages/opening.py
+++ pages/opening.py
@@ -13,21 +13,19 @@ content = html.Img(
 )
 
 description = html.Div([
-    #html.H5("Lorem Ipsum", className="text-info"),
+    html.H5("Why..?", className="text-info"),
     html.P("""
-    Why is a 40+ years old song still topping the billboard charts today? Yes. We are referring to Bohemian Rhapsody by 
-    the famous British Rock band Queen. According to the Billboard ranking’s in the Netherlands Bohemian along with a 
+    ...is a 40+ years old song still topping the billboard charts today? Yes. We are referring to Bohemian Rhapsody by 
+    the famous British Rock band Queen. 
+    """),
+    html.P("""
+    According to the Billboard ranking’s in the Netherlands Bohemian Rhapsody along with a 
     few other tracks such Hotel California and Imagine stands in the top three rankings of the billboard from 1999 to 2019. 
-    Interestingly those are all Rock songs released before 1990. Does this mean that old music is better, or is new music 
+    Interestingly, these are all Rock songs released before 1990. Does this mean that old music is better, or is new music 
     not as great? Looks like we will have to dig deep to find answers to this phenomenon.
     """),
     html.P("""
-    We developed a tool that visualizes a collection of charts that could help analyze the billboard data of the top 2000 
-    songs in the Netherlands along with additional data sources which could support to elaborate further the factors that 
-    makes music rank higher or lower in the billboard.
-    """),
-    html.P("""
-    So, sit back and cruise through the rocking journey down the billboard isle!
+    So, sit back and cruise through this rocking journey down the billboard isle!
     """),
 
 ])
